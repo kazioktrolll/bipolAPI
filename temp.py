422a2e35e2f439b5004d885af6cd05e41cff7e68
Kazioktroll <03woj.kwi03@gmail.com>
2024-12-22 19:27:57 +0100

    changed the 'trial_scene' from function to class

--- temp.py
+++ temp.py
@@ -2,29 +2,29 @@ import customtkinter as ctk
 from Graphics import App, HoverButton, ScrolledText
 
 
-def trial_scene(master: App):
-    scene = ctk.CTkFrame(master)
-    scene.columnconfigure(0, weight=1)
-    scene.rowconfigure(0, weight=1)
-    scene.rowconfigure(1, weight=0)
-
-    scene.output_area = ScrolledText(scene)
-    scene.output_area.grid(row=0, column=0, sticky="nsew")
-
-    lower_layout = ctk.CTkFrame(scene, height=100)
-    lower_layout.grid(row=1, column=0, sticky="nsew")
-    scene.command_input_field = ctk.CTkEntry(lower_layout, width=40)
-    scene.command_input_field.grid(row=0, column=0)
-    HoverButton(
-        lower_layout,
-        text="Send",
-        hover_text="Send current contents of input field as a command to the console.",
-        command=lambda:scene.master.main.send_command(scene.command_input_field.get() + "\n")
-    ).grid(row=0, column=1)
-    HoverButton(
-        lower_layout,
-        text="Quit",
-        hover_text="Quit current console app.",
-        command=lambda: scene.master.main.send_command("QUIT\n")
-    ).grid(row=0, column=2)
-    return scene
+class TrialScene(ctk.CTkFrame):
+    def __init__(self, master: App):
+        super().__init__(master)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=0)
+    
+        self.output_area = ScrolledText(self)
+        self.output_area.grid(row=0, column=0, sticky="nsew")
+    
+        lower_layout = ctk.CTkFrame(self, height=100)
+        lower_layout.grid(row=1, column=0, sticky="nsew")
+        self.command_input_field = ctk.CTkEntry(lower_layout, width=40)
+        self.command_input_field.grid(row=0, column=0)
+        HoverButton(
+            lower_layout,
+            text="Send",
+            hover_text="Send current contents of input field as a command to the console.",
+            command=lambda:self.master.main.send_command(self.command_input_field.get() + "\n")
+        ).grid(row=0, column=1)
+        HoverButton(
+            lower_layout,
+            text="Quit",
+            hover_text="Quit current console app.",
+            command=lambda: self.master.main.send_command("QUIT\n")
+        ).grid(row=0, column=2)
