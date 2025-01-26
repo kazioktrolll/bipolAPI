ae308b2ebbca3840ceee673ff49ff831ff9fcf95
Kazioktroll <03woj.kwi03@gmail.com>
2025-01-26 21:42:49 +0100

    added documentation

--- src/scenes/scene.py
+++ src/scenes/scene.py
@@ -3,9 +3,30 @@ from ..app import App
 
 
 class Scene(CTkFrame):
+    """
+    A base class for creating and managing application scenes.
+
+    A Scene represents a single state or view of the application's UI.
+    For example:
+        - The initial Scene displayed when the program starts.
+        - A subsequent Scene shown after user interaction, such as pressing a 'Load File' button,
+          which transitions the app to a working Scene with different widgets and functionalities.
+
+    Attributes:
+        app (App): The application instance.
+
+    Methods:
+        build(): Constructs and arranges the widgets to be displayed in the Scene.
+        This method should be overridden in subclasses to define the layout and components for each specific Scene.
+    """
     def __init__(self, app:App):
         super().__init__(app.root)
         self.app = app
         self.build()
 
-    def build(self) -> None: pass
+    def build(self) -> None:
+        """
+        Constructs and arranges the widgets to be displayed in the Scene.
+        This method should be overridden in subclasses to define the layout and components for each specific Scene.
+        """
+        pass
