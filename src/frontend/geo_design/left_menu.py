4dac9e648e28f8b4c74416580f47424023ee5f65
kazioktrolll <kwiatk.woj@gmail.com>
2025-02-27 16:33:33 +0100

    Changed LeftMenu so it now shows all the surfaces automatically. Works only for Simple and Vertical Surfaces for now.

--- src/frontend/geo_design/left_menu.py
+++ src/frontend/geo_design/left_menu.py
@@ -1,9 +1,10 @@
 from customtkinter import CTkFrame, CTkButton, ThemeManager
 from typing import Callable
 
+from .left_menu_item import LeftMenuItem
 from .left_menu_simple_surface import LeftMenuSimpleSurface
 from .left_menu_vertical_surface import LeftMenuVerticalSurface
-from ...backend.geo_design import Geometry
+from ...backend.geo_design import Geometry, SimpleSurface, VerticalSurface
 
 
 class LeftMenu(CTkFrame):
@@ -11,30 +12,33 @@ class LeftMenu(CTkFrame):
         super().__init__(parent)
 
         self._do_on_update = do_on_update
+        self.items: dict[str, LeftMenuItem] = {}
+        self.buttons: dict[str, CTkButton] = {}
+        self.buttons_frame = CTkFrame(self, fg_color=self.cget("fg_color"))
 
+        self.update_items()
+        self.build()
+        self.show_first()
+
+    def build(self) -> None:
         self.columnconfigure(0, weight=1)
         self.rowconfigure(2, weight=1)
 
-        self.buttons = CTkFrame(self, fg_color=self.cget("fg_color"))
-        self.buttons.columnconfigure(0, weight=1)
-        self.buttons.columnconfigure(1, weight=1)
-        self.buttons.columnconfigure(2, weight=1)
-        self.buttons.rowconfigure(0, minsize=4)
-        self.buttons.grid(row=0, column=0, sticky='nsew')
-        CTkButton(self.buttons, text="Wing", command=self.show_wing, width=50
-                  ).grid(row=1, column=0, sticky='nsew')
-        CTkButton(self.buttons, text="Ver. Tail", command=self.show_v_tail, width=50
-                  ).grid(row=1, column=1, sticky='nsew', padx=5)
-        CTkButton(self.buttons, text="Hor. Tail", command=self.show_h_tail, width=50
-                  ).grid(row=1, column=2, sticky='nsew')
+        self.buttons_frame.grid(row=0, column=0, sticky='nsew')
+        self.buttons_frame.children.clear()
+
+        for i, (name, item) in enumerate(self.items.items()):
+            self.buttons[name] = CTkButton(self.buttons_frame, text=name, width=40, command=lambda: self.show_item(name))
+            self.buttons[name].grid(row=1, column=i, sticky="nsew")
+            self.buttons_frame.columnconfigure(i, weight=1)
 
         self.rowconfigure(1, minsize=4)
 
-        self.wing = LeftMenuSimpleSurface.default(self, 'wing')
-        self.v_tail = LeftMenuVerticalSurface.default(self, 'v_tail')
-        self.h_tail = LeftMenuSimpleSurface.default(self, 'h_tail')
-
-        self.show_wing()
+    def update_items(self) -> None:
+        menu_item = {SimpleSurface: LeftMenuSimpleSurface,
+                     VerticalSurface: LeftMenuVerticalSurface}
+        for name, surface in self.geometry.surfaces.items():
+            self.items[name] = menu_item[type(surface)](self, surface)
 
     @property
     def geometry(self) -> Geometry:
@@ -45,32 +49,20 @@ class LeftMenu(CTkFrame):
     def do_on_update(self) -> None:
         self._do_on_update()
 
-    def show_wing(self):
-        if self.wing.grid_info() != {}: return
-        self.v_tail.place(x=-1e4, y=-1e4)
-        self.h_tail.place(x=-2e4, y=-2e4)
-        self.wing.grid(row=2, column=0, sticky='nsew', padx=10)
-        self.wing_button.configure(fg_color=ThemeManager.theme["CTkButton"]["fg_color"], state="enabled")   # noqa
-        self.v_tail_button.configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")   # noqa
-        self.h_tail_button.configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")   # noqa
+    def show_item(self, item: str) -> None:
+        if self.items[item].grid_info() != {}: return
 
-    def show_v_tail(self):
-        if self.v_tail.grid_info() != {}: return
-        self.wing.place(x=-1e4, y=-1e4)
-        self.h_tail.place(x=-2e4, y=-2e4)
-        self.v_tail.grid(row=2, column=0, sticky='nsew', padx=10)
-        self.wing_button.configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")   # noqa
-        self.v_tail_button.configure(fg_color=ThemeManager.theme["CTkButton"]["fg_color"], state="enabled")   # noqa
-        self.h_tail_button.configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")   # noqa
+        for i, name in enumerate(self.items.keys()):
+            if name == item:
+                self.items[name].grid(row=2, column=0, sticky='nsew', padx=10)
+                self.buttons[name].configure(fg_color=ThemeManager.theme["CTkButton"]["fg_color"], state="enabled") # noqa
+                continue
+            self.items[name].place(x=-i*1e4, y=-1e4)
+            self.buttons[name].configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")       # noqa
 
-    def show_h_tail(self):
-        if self.h_tail.grid_info() != {}: return
-        self.wing.place(x=-1e4, y=-1e4)
-        self.v_tail.place(x=-2e4, y=-2e4)
-        self.h_tail.grid(row=2, column=0, sticky='nsew', padx=10)
-        self.wing_button.configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")  # noqa
-        self.v_tail_button.configure(fg_color=ThemeManager.theme["CTkFrame"]["fg_color"], state="normal")  # noqa
-        self.h_tail_button.configure(fg_color=ThemeManager.theme["CTkButton"]["fg_color"], state="enabled")  # noqa
+    def show_first(self) -> None:
+        first = tuple(self.items.keys())[-1]
+        self.show_item(first)
 
     @property
     def wing_button(self):
