2a67c920ea8edb426d2ee79205cee1b4620bab24
kazioktrolll <kwiatk.woj@gmail.com>
2025-03-01 22:38:00 +0100

    Added TopBarItem widget for creating a dropdown-based top bar menu in the App.

--- src/frontend/__init__.py
+++ src/frontend/__init__.py
@@ -4,3 +4,4 @@ from .items import Item, FlapItem
 from .help_top_level import HelpTopLevel
 from .geo_design import *
 from .popup import Popup
+from .top_bar import TopBarItem
---
+++ src/frontend/top_bar.py
@@ -0,0 +1,35 @@
+from customtkinter import CTkFrame, CTkButton
+from typing import Callable
+
+
+class TopBarItem(CTkButton):
+    def __init__(self, parent, root, name:str, options:list[tuple[str, Callable[[], None]]]):
+        self.options = options
+        super().__init__(
+            parent, text=name, command=self.toggle_menu, corner_radius=0, fg_color=parent.cget('bg_color')
+        )
+        self.dropdown = CTkFrame(root)
+        self.update()
+
+    def add_option(self, name:str, func:Callable[[], None]) -> None:
+        self.options.append((name, func))
+        self.update()
+
+    def update(self):
+        for name, func in self.options:
+            CTkButton(self.dropdown, text=name,
+                      command=func,
+                      fg_color=self.dropdown.cget("fg_color")).pack()
+
+    def toggle_menu(self):
+        if self.dropdown.winfo_ismapped():
+            self.dropdown.place_forget()
+        else:
+            x = self.winfo_vrootx()
+            y = self.winfo_vrooty() + self.winfo_height()
+            self.dropdown.place(x=x, y=y, anchor='nw')
+            self.dropdown.lift()
+
+    def collapse(self):
+        if self.dropdown.winfo_ismapped():
+            self.dropdown.place_forget()
