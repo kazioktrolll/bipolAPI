59123f2237721f0acbd8c50a8d91464937109f37
kazioktrolll <kwiatk.woj@gmail.com>
2025-03-06 19:56:32 +0100

    Reworked OperInput name keeping.

--- src/calcs/frontend/oper_input.py
+++ src/calcs/frontend/oper_input.py
@@ -6,28 +6,29 @@ Gridable = CTk | CTkFrame
 
 
 class OperInput(CTkFrame):
-    def __init__(self, master: Gridable, name: str, key: str, master_grid = False, master_row=0, control_surfaces: list[str] = None):
+    def __init__(self, master: Gridable,
+                 key: str, name: str = None,
+                 master_grid = False, master_row=None,
+                 control_surfaces: list[str] = None):
+
         super().__init__(master)
         self.value = DoubleVar(value=0.0)
-        self.key = key
+
         self.master_grid = master if master_grid else self
-        self.master_row = master_row
-        self.bind_options = {
-                    'Alpha': 'a',
-                    'Beta': 'b',
-                    'Roll Rate': 'r',
-                    'Pitch Rate': 'p',
-                    'Yaw Rate': 'y',
-                    'CL': 'c',
-                    'CY': 's',
-                    'Roll Moment': 'rm',
-                    'Pitching Moment': 'pm',
-                    'Yaw Moment': 'ym'
-                }
-        if name in self.bind_options: del self.bind_options[name]
-        if control_surfaces: self.bind_options |= {cs: f'd{i}' for i, cs in enumerate(control_surfaces)}
-        self.name_label = CTkLabel(self.master_grid, text=name)
-        self.bind_menu = CTkOptionMenu(self.master_grid, width=80, values=[name for name, _ in self.bind_options.items()])
+        self.master_row = master_row if master_grid else 0
+
+        self.bind_options = {opt.avl: opt for opt in NameKeeper.full_set()}
+        if key in NameKeeper.builtins():
+            self.name = NameKeeper(builtin=key)
+        else:
+            self.name = NameKeeper(name=name, dx=key)
+        if control_surfaces:
+            self.bind_options |= {f'd{i}': NameKeeper(name=surf_name, dx=f'd{i}') for i, surf_name in enumerate(control_surfaces)}
+        if self.name.avl in self.bind_options.keys():
+            del self.bind_options[self.name.avl]
+
+        self.name_label = CTkLabel(self.master_grid, text=self.name.display)
+        self.bind_menu = CTkOptionMenu(self.master_grid, width=80, values=[opt.display for opt in self.bind_options.values()])
         self.value_label = CTkLabel(self.master_grid, textvariable=self.value)
         self.entry = CTkEntry(self.master_grid)
         self.set_button = CTkButton(self.master_grid, text="Set", width=30, command=self.set_value)
@@ -69,21 +70,51 @@ class OperInput(CTkFrame):
         super().grid(**kwargs)
 
     @classmethod
-    def by_template(cls, master:Gridable, template: Literal['a', 'b', 'r', 'p', 'y']|str,
-                    master_grid = False, master_row=0,
-                    control_surfaces: list[str] = None) -> 'OperInput':
-        name = {'a':'Alpha', 'b':'Beta', 'r':'Roll Rate', 'p':'Pitch Rate', 'y':'Yaw Rate'}[template]
-        return cls(master=master, name=name, key=template, master_grid=master_grid, master_row=master_row, control_surfaces=control_surfaces)
-
-    @classmethod
     def full_set(cls, master: Gridable, master_grid = False, control_surfaces: list[str] = None) -> list['OperInput']:
         if master_grid is None:
-            return [cls.by_template(master=master, template=t, control_surfaces=control_surfaces)
+            return [cls(master=master, key=t, control_surfaces=control_surfaces)
                     for t in ['a', 'b', 'r', 'p', 'y']]
-        return [cls.by_template(master=master, template=t, master_grid=master_grid, master_row=i, control_surfaces=control_surfaces)
+        return [cls(master=master, key=t, master_grid=master_grid, master_row=i, control_surfaces=control_surfaces)
                 for i, t in enumerate(['a', 'b', 'r', 'p', 'y'])]
 
     def command_string(self) -> str:
-        if not self.bound: return f"{self.key} {self.key} {self.value.get()}"
-        bound_key = self.bind_options[self.bind_menu.get()]
-        return f"{self.key} {bound_key} {self.value.get()}"
+        if not self.bound:
+            return f"{self.name.avl} {self.name.avl} {self.value.get()}"
+        bound = self.bind_options[self.bind_menu.get()]
+        return f"{self.name.avl} {bound.avl} {self.value.get()}"
+
+    def run_file_string(self) -> str:
+        if not self.bound:
+            return f"{self.name.run_file} -> {self.name.run_file} = {self.value.get()}"
+        bound = self.bind_options[self.bind_menu.get()]
+        return f"{self.name.run_file} -> {bound.run_file} = {self.value.get()}"
+
+
+class NameKeeper:
+    def __init__(self, builtin: Literal['a', 'b', 'r', 'p', 'y']|str=None, name:str = None, dx:str = None):
+        self.letter = builtin
+        self.name = name
+        self.dx = dx
+
+    @property
+    def avl(self) -> str:
+        if self.dx: return self.dx
+        return str(self.letter)
+
+    @property
+    def display(self):
+        if self.name: return self.name
+        return {'a':'Alpha', 'b':'Beta', 'r':'Roll Rate', 'p':'Pitch Rate', 'y':'Yaw Rate'}[self.letter]
+
+    @property
+    def run_file(self):
+        if self.name: return self.name.capitalize()
+        return {'a':'alpha', 'b':'beta', 'r':'pb/2V', 'p':'qc/2V', 'y':'rb/2V'}[self.letter]
+
+    @classmethod
+    def builtins(cls):
+        return ['a', 'b', 'r', 'p', 'y']
+
+    @classmethod
+    def full_set(cls):
+        return [cls(builtin=l) for l in cls.builtins()]
