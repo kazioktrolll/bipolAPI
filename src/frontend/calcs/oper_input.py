bc84eced8e6d51ce35976b4b9f8e9d3ca4c7e8de
kazioktrolll <kwiatk.woj@gmail.com>
2025-03-11 18:51:48 +0100

    Made OperInput inherit from RowManager.

--- src/frontend/calcs/oper_input.py
+++ src/frontend/calcs/oper_input.py
@@ -1,20 +1,29 @@
 from customtkinter import CTkFrame, CTkLabel, CTkButton, CTkEntry, CTkOptionMenu, CTk, DoubleVar
+from ..strip_manager import RowManager
 
 
 Gridable = CTk | CTkFrame
 
 
-class OperInput(CTkFrame):
-    def __init__(self, master: Gridable, name: str,
-                 master_grid = False, master_row:int=None,
-                 control_surfaces: list[str] = None):
+class OperInput(RowManager):
+    base_names = {
+        'Alpha': ('A', 'alpha'),
+        'Beta': ('B', 'beta'),
+        'Roll Rate': ('R', 'pb/2V'),
+        'Pitch Rate': ('P', 'qc/2V'),
+        'Yaw Rate': ('Y', 'rb/2V')
+    }
+    bindable_names = {
+        'CL': ('c', 'CL'),
+        'CY': ('s', 'CY'),
+        'Roll Moment': ('rm', 'Cl roll mom'),
+        'Pitch Moment': ('pm', 'Cm pitchmom'),
+        'Yaw Moment': ('ym', 'Cn yaw mom')
+    }
 
-        super().__init__(master)
+    def __init__(self, grid: Gridable, name: str, master_row:int=None, control_surfaces: list[str] = None):
+        super().__init__(grid, master_row)
         self.value = DoubleVar(value=0.0)
-
-        self.master_grid = master if master_grid else self
-        self.master_row = master_row if master_grid else 0
-
 
         self.base_names = {
             'Alpha': ('A', 'alpha'),
@@ -35,15 +44,15 @@ class OperInput(CTkFrame):
         self.command_name = self.base_names[name][0]
         self.run_file_name = self.base_names[name][1]
 
-        self.name_label = CTkLabel(self.master_grid, text=self.display_name)
-        self.bind_menu = CTkOptionMenu(self.master_grid, width=80, values=list(self.bindable_names.keys()))
-        self.value_label = CTkLabel(self.master_grid, textvariable=self.value)
-        self.entry = CTkEntry(self.master_grid)
-        self.set_button = CTkButton(self.master_grid, text="Set", width=30, command=self.set_value)
-        self.bind_button = CTkButton(self.master_grid, text="Bind", width=20, command=self.bind_switch)
-        self.bound = False
-        self.initialized = False
+        self.name_label = CTkLabel(self.grid, text=self.display_name, anchor='e')
+        self.bind_menu = CTkOptionMenu(self.grid, width=120, values=list(self.bindable_names.keys()))
+        self.series_manager = SeriesConfig(self.grid)
+        self.value_label = CTkLabel(self.grid, textvariable=self.value)
+        self.entry = CTkEntry(self.grid)
+        self.set_button = CTkButton(self.grid, text="Set", width=30, command=self.set_value)
+        self.bind_button = CTkButton(self.grid, text="Bind", width=20, command=self.bind_switch)
 
+        self.bound = False
         self.build()
 
     def set_value(self, value:float=None):
@@ -51,31 +60,33 @@ class OperInput(CTkFrame):
             try:
                 self.set_value(float(self.entry.get()))
                 self.entry.delete(0, 'end')
-                self.focus_set()
+                self.grid.focus_set()
                 return
             except ValueError: return
         self.value.set(value)
 
     def bind_switch(self):
         self.bound = not self.bound
-        self.build()
+        self.update()
+
+    def update(self):
+        if not self.bound: self.bind_menu.grid_forget()
+        else: self.bind_menu.grid(column=1, row=self.master_index)
 
     def build(self):
-        r = self.master_row
-        if not self.bound: self.bind_menu.grid_forget()
-        else: self.bind_menu.grid(row=r, column=1)
-
-        if self.initialized: return
-        self.name_label.grid(row=r, column=0)
-        self.value_label.grid(row=r, column=2, padx=5)
-        self.entry.grid(row=r, column=3)
-        self.set_button.grid(row=r, column=4)
-        self.bind_button.grid(row=r, column=5)
-        self.initialized = True
-
-    def grid(self, **kwargs):
-        if self.master_grid is not self: raise PermissionError('Cannot grid a OperInput if master_grid was given!')
-        super().grid(**kwargs)
+        self.grid.columnconfigure(1, minsize=120)
+        self.stack(self.name_label, sticky='e')
+        self.stack([
+            self.bind_menu,
+            self.series_manager,
+        ])
+        self.stack(self.value_label, padx=5)
+        self.stack([
+            self.entry,
+            self.set_button,
+            self.bind_button
+        ])
+        self.update()
 
     def command_string(self) -> str:
         if not self.bound:
@@ -94,13 +105,19 @@ class OperInputPanel(CTkFrame):
     def __init__(self, parent: Gridable, control_surfaces: list[str] = None):
         super().__init__(parent)
         self.ois: list[OperInput] = [
-            OperInput(master=self, name='Alpha', master_grid=True, master_row=0, control_surfaces=control_surfaces)
+            OperInput(grid=self, name='Alpha', master_row=0, control_surfaces=control_surfaces)
         ]
         for i, name in enumerate(self.ois[0].base_names.keys()):
             if i == 0: continue
             self.ois.append(
-                OperInput(master=self, name=name, master_grid=True, master_row=i, control_surfaces=control_surfaces)
+                OperInput(grid=self, name=name, master_row=i, control_surfaces=control_surfaces)
             )
 
     def run_file_string(self) -> str:
         return "\n".join([oi.run_file_string() for oi in self.ois]) + "\n"
+
+
+class SeriesConfig(CTkFrame):
+    def __init__(self, parent: Gridable):
+        super().__init__(parent)
+        CTkOptionMenu(self, values=['Constant', 'Range', 'From File']).grid(column=0, row=0)
