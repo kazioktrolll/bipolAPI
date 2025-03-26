93091c6c99ff61d0bc94f0e43737f740c9839a0d
kazioktrolll <kwiatk.woj@gmail.com>
2025-03-26 20:37:24 +0100

    Created load_from_csv.py to, well, load data from .csv files.

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -2,3 +2,4 @@ from . import physics
 from . import geo_design
 from .avl_interface import AVLInterface, ResultsParser
 from .vector3 import Vector3, AnyVector3
+from .load_from_csv import load_from_csv
---
+++ src/backend/load_from_csv.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+
+def load_from_csv(path: Path | str) -> dict[str, list[float]] | list[list[float]]:
+    if isinstance(path, str): path = Path(path)
+    with open(path) as f: data = f.readlines()
+    raw_vals = [line.split(',') for line in data]
+    try:
+        float(raw_vals[0][0])
+        return to_columns(raw_vals)
+    except ValueError:
+        return to_dict(raw_vals)
+
+
+def to_columns(raw_vals: list[list[str]]) -> list[list[float]]:
+    float_rows = [[float(val) for val in line] for line in raw_vals]
+    columns = []
+    for i in range(len(raw_vals[0])): columns.append([.0] * len(raw_vals))
+    for i in range(len(float_rows)):
+        for j in range(len(float_rows[0])):
+            columns[j][i] = float_rows[i][j]
+    return columns
+
+
+def to_dict(raw_vals: list[list[str]]) -> dict[str, list[float]]:
+    keys = raw_vals.pop(0)
+    cols = to_columns(raw_vals)
+    return {k: c for k, c in zip(keys, cols)}
