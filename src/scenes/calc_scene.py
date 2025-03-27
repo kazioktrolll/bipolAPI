41d1af424e46687a34e746969b5ea895f9e56328
kazioktrolll <kwiatk.woj@gmail.com>
2025-03-27 09:31:13 +0100

    Simplified CalcScene

--- src/scenes/calc_scene.py
+++ src/scenes/calc_scene.py
@@ -1,15 +1,17 @@
+from functools import cached_property
 from .scene import Scene
 from ..frontend import CalcDisplay
 
 
 class CalcScene(Scene):
     def __init__(self, app):
-        self.display = None
         super().__init__(app)
+
+    @cached_property
+    def display(self):
+        return CalcDisplay(self)
 
     def build(self) -> None:
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-
-        self.display = CalcDisplay(self)
         self.display.grid(row=0, column=0)
