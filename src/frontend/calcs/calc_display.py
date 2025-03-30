76c0ed2c024745921f6cb50a92a85b79c8af3a60
kazioktrolll <kwiatk.woj@gmail.com>
2025-03-30 21:11:47 +0200

    Implemented last commit, also cleaned up.

--- src/frontend/calcs/calc_display.py
+++ src/frontend/calcs/calc_display.py
@@ -8,8 +8,11 @@ class CalcDisplay(CTkFrame):
         super().__init__(parent)
         self.ois_frame = None
         self.exec_button = None
-        self.results_display: ResultsDisplay | None = None
-        self.oip: OperSeriesInputPanel | None = None
+        self.results_display = ResultsDisplay(self, self.geometry.get_controls())
+
+        controls_names = [c.name for c in self.geometry.get_controls()]
+        self.oip = OperSeriesInputPanel(self, controls_names)
+
         self.build()
 
     @property
@@ -21,18 +24,14 @@ class CalcDisplay(CTkFrame):
         return self.master.app.geometry
 
     def build(self):
-        from .oper_input import OperSeriesInputPanel
-        controls = self.geometry.get_controls()
-        controls_names = [c.name for c in controls]
-        self.oip = OperSeriesInputPanel(self, controls_names)
         self.oip.grid(row=0, column=0, sticky="nsew")
 
         from customtkinter import CTkButton
         self.exec_button = CTkButton(self, text='Execute', command=self.run_case)
         self.rowconfigure(2, weight=1)
         self.exec_button.grid(row=1, column=0, sticky='news')
-        self.results_display = ResultsDisplay(self, self.geometry.get_controls())
         self.columnconfigure(1, minsize=10)
+
         self.results_display.grid(row=0, column=2, rowspan=2, sticky='news')
         self.run_case()
 
@@ -45,6 +44,6 @@ class CalcDisplay(CTkFrame):
 
         self.exec_button.configure(state='disabled')
         data = self.oip.get_run_file_data()
-        vals = AVLInterface.run_series(self.geometry, data)
+        vals, errors = AVLInterface.run_series(self.geometry, data)
         self.results_display.set_results(vals)
         self.exec_button.configure(state='normal')
